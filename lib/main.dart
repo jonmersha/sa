8f3d2f421ee6c1a05033930773e065640117acf8
jonmersha@gmail.com <jonmersha@gmail.com>
2023-10-06 22:53:01 +0300

    icon added

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:awashderash/auth/fire_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 
+
 import 'firebase_options.dart';
 
 //import { getAuth, onAuthStateChanged } from "firebase/auth";
@@ -13,13 +14,10 @@ void main() async {
 
   runApp(MaterialApp(
     initialRoute: '/',
-    theme: ThemeData().copyWith(
-        useMaterial3: true,
-        colorScheme: ColorScheme.fromSeed(
-            seedColor: const Color.fromARGB(255, 2, 57, 144))),
     routes: {
       '/': (context) => const AuthGate(),
     },
     debugShowCheckedModeBanner: false,
   ));
 }
+
